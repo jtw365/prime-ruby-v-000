5a13becc24989ed7755bb71d179a46581bfc99bd
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-23 11:24:15 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,10 @@
 # Add  code here!
 def prime?(num)
-  if num <= 3
+  if num <= 1
     return false
+  elsif num <=3
+    return true
   else (2..n/2).none? do |x|
     n % x == 0
   end
 end
-end
-
